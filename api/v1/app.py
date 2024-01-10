730b489e20554d4a491074a74a35eb08f440d82a
Sunkade <sunkadefasola@gmail.com>
2024-01-10 04:34:05 +0100

    Commit message describing your changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -39,7 +39,9 @@ def handle_404(exception):
     resp = jsonify(data)
     resp.status_code = 404
 
-    return(resp)
+    return resp
 
 if __name__ == "__main__":
-    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(getenv("HBNB_API_PORT", 5000))
+    app.run(host=host, port=port)
